6f331942d757b048ae045aa29e4c6b52c057c460
Ivan Vasilev <tor419@yandex.ru>
2023-03-31 15:10:46 +0700

    chore: add tests commit

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,21 +1,99 @@
 package ru.netology.rest;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 
 class MobileBankApiTestV1 {
     @Test
-    void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200);
+    void shouldReturnDemoAccountsStatusCode() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void shouldReturnDemoAccountsContentTypeJSON() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .contentType(ContentType.JSON);
+    }
+
+    @Test
+    void shouldReturnDemoAccountsBodyThreeObject() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // специализированные проверки - лучше
+                .body("", hasSize(3))
+        ;
+    }
+
+    @Test
+    void shouldReturnDemoAccountsBodyInObjectTwoUsd() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body("[1].currency", equalTo("USD"))
+        ;
+    }
+
+    @Test
+    void shouldReturnDemoAccountsBodyBalanceIsPositive() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body(" every { it.balance >= 0 }", is(true))
+        ;
+    }
+
+    @Test
+    void shouldReturnDemoAccountsBody() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
     }
 }
+
